251563432241239636e857a319b0b97d88a9fdfd
Hallstead <38665384+Hallstead@users.noreply.github.com>
2020-05-13 09:58:01 -0600

    finished user table functionality

--- src/daos/user-dao.ts
+++ src/daos/user-dao.ts
@@ -1,17 +1,15 @@
 import { db } from './db';
 import { User, UserRow } from '../models/User';
 
-export function getUserByName(name: string): Promise<boolean> {
-    const sql = 'select name FROM users WHERE name = $1';
+export function getUserByName(name: string): Promise<User> {
+    const sql = 'select * FROM project0.users WHERE name = $1';
 
-    return db.query<UserRow>(sql, name).then(result => result.rows.map(row => User.from(row))[0]);
+    return db.query<UserRow>(sql, [name]).then(result => result.rows.map(row => User.from(row))[0]);
 }
 
 export function saveUser(user: User): Promise<User> {
-    const sql = `INSERT INTO users (name, last_name, birthdate) \
-VALUES ($1, $2, $3) RETURNING *`;
+    
+    const sql = `INSERT INTO project0.users (name) VALUES ($1) RETURNING *`;
 
-    return db.query<UserRow>(sql, [
-        user.name,
-    ]).then(result => result.rows.map(row => User.from(row))[0]);
+    return db.query<UserRow>(sql, [user.name]).then(result => result.rows.map(row => User.from(row))[0]);
 }
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,27 +1,24 @@
 import { User } from '../models/User';
 import * as userDao from '../daos/User-dao';
+import { resolve } from 'dns';
 
-export function getUserByName(name: string) {
+export async function getUserByName(name: string): Promise<User> {
     return userDao.getUserByName(name);
 }
 
-export function saveUser(user: any): Promise<User> {
-
-    console.log(user);
+export async function saveUser(user: any): Promise<User> {
     // Data from the user cannot be trusted
     const newUser = new User(
         undefined, user.name
     );
-
-    // IF we're going validate it here, we probably want
-    // constraints on the db too
-
-    if(getUserByName(user)) {
+    //check if user exists
+    const promise = await getUserByName(newUser.name);
+    if(!promise) {
         // Data is valid - Continue submitting to DAO
         return userDao.saveUser(newUser);
     } else {
         // TODO: We should fail here, probably issue some kind of 400
         console.warn('User already exists');
-        return new Promise((resolve, reject) => reject(422));
+        return new Promise((resolve, reject) => reject(409));
     }
 }
